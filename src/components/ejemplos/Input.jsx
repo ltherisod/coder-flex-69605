11763649c45751f14254e0a1815093118705dd4e
ltherisod <lauratherisod@gmail.com>
2025-05-08 21:08:39 -0300

    eventos

---
+++ src/components/ejemplos/Input.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const Input = () => {
+    const [nombre, setNombre] = useState('')
+
+    const inputHandler = (event) => {
+        console.log(event.target.value)
+        setNombre(event.target.value)
+    }
+
+    const onKeyDownHandler = (e) => {
+       if('aeiou'.includes(e.key.toLowerCase())){
+        console.log('es vocal')
+        e.preventDefault()
+       }else{
+        console.log('no es vocal')
+       }
+    }
+  return (
+    <div>
+        <input placeholder='complete con su nombre completo' onChange={inputHandler} type='text' name='nombre-completo'/>
+        {/* <input placeholder='complete con su nombre completo' onChange={(e)=> setNombre(e.target.value)} type='text' name='nombre-completo'/> */}
+        <p>{nombre}</p>
+        <h1>Actividad de no vocales</h1>
+        <input placeholder='No vocals!' type='text' onKeyDown={onKeyDownHandler}/>
+    </div>
+  )
+}
+
+export default Input
